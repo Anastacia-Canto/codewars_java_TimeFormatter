68c93b14b2fb25ac6ca022e44cd1b0331c5ad4ad
Anastácia Canto <91332251+Anastacia-Canto@users.noreply.github.com>
2022-08-18 20:44:31 +0100

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("100: " + TimeFormatter.formatDuration(100));
+		System.out.println("1: " + TimeFormatter.formatDuration(1));
+		System.out.println("0: " + TimeFormatter.formatDuration(0));
+		System.out.println("247: " + TimeFormatter.formatDuration(247));
+		System.out.println("90000: " + TimeFormatter.formatDuration(90000));
+		System.out.println("3662: " + TimeFormatter.formatDuration(3662));
+		System.out.println("3600: " + TimeFormatter.formatDuration(3600));
+		
+
+	}
+
+}
